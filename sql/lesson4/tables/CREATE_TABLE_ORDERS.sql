f02590d929dcae67b98f8f001df2bf6c730278bb
DaNaRim <rangar.danarim@gmail.com>
2022-03-21 16:29:17 +0200

    sql files cleanup

--- sql/lesson4/tables/CREATE_TABLE_ORDERS.sql
+++ sql/lesson4/tables/CREATE_TABLE_ORDERS.sql
@@ -5,7 +5,7 @@ CREATE TABLE orders
     room_id    NUMBER,
     date_from  DATE NOT NULL,
     date_to    DATE NOT NULL,
-    money_paid number(*, 2),
+    money_paid NUMBER(*, 2),
     CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES users (id),
     CONSTRAINT room_fk FOREIGN KEY (room_id) REFERENCES room (id)
 );
